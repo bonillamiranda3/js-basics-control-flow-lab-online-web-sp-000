4682cffa37b80d835b73edf2b653c79c0410ebaf
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-01 03:24:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Write your code in this file!
+
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
     return "This one is on me!"
-  } else if (2000 < feet && feet <= 2500) {
+  } else if (feet > 2000 && feet <= 2500) {
     return 'I will gladly take your thirty bucks.'
   } else {
     return "No can do."
@@ -10,18 +11,16 @@ function scuberGreetingForFeet(feet) {
 }
 
 function ternaryCheckCity(city) {
-  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
 function switchOnCharmFromTip(tip) {
   switch (tip) {
-    case 'generous'
-      return "Thank you so much.";
-      break;
-    case 'not as generous';
-        return "Thank you.";
-          break;
-    case "Thank you."
-          return "Bye."
+    case 'generous':
+  return 'Thank you so much.';
+    case 'not as generous':
+  return 'Thank you.';
+    case 'Thanks!':
+  return 'Bye.';
   }
 }
