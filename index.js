4430d88ba2725df0de960f01a3d29a21c25a8037
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-01 03:26:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function switchOnCharmFromTip(tip) {
   return 'Thank you so much.';
     case 'not as generous':
   return 'Thank you.';
-    case 'Thanks!':
+    default:
   return 'Bye.';
   }
 }
